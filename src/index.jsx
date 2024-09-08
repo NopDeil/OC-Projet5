58c2acc32727f8a16ce4915eda6f68ce5b3b46f0
Noa <noa.houssin@hotmail.com>
2024-09-08 13:00:18 +0200

    déplacement logique de route app.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Home from "./pages/Home/Home";
@@ -7,6 +7,7 @@ import Accommodation from "./pages/Accommodation/Accommodation";
 import Error from "./pages/Error/Error";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import App from "./app";
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -14,12 +15,7 @@ root.render(
   <Router>
     <Header />
     <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/accommodation/:id" element={<Accommodation />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
+      <App />
     </main>
     <Footer />
   </Router>
